a84676e38902c4f254f4c6ea51c162167c631811
YEJIN-GIT <yejinaissi@gmail.com>
2026-05-19 15:45:27 +0900

    260519 [develop] init

---
+++ src/main/java/com/msa4meerketgram/Msa4MeerketgramApplication.java
@@ -0,0 +1,13 @@
+package com.msa4meerketgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4MeerketgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4MeerketgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msa4meerketgram/Msa4MeerketgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa4meerketgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4MeerketgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
